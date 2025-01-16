2480f3b7c50f5247b69519907c9d70a6262dd1ba
SVO <svo@qual.is>
2025-01-17 09:03:42 +1100

    Removing unused modalStyle

--- src/app/_components/modal/contact-modal.tsx
+++ src/app/_components/modal/contact-modal.tsx
@@ -6,17 +6,6 @@ import Modal from 'react-modal';
 
 import "./modal.css";
 
-const modalStyle = {
-  content: {
-    top: '50%',
-    left: '50%',
-    right: 'auto',
-    bottom: 'auto',
-    marginRight: '-50%',
-    transform: 'translate(-50%, -50%)',
-  },
-};
-
 Modal.setAppElement('#__next');
 
 const ContactModal = () => {
