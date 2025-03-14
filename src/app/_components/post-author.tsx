9a97b5fc04c341150458be8f3eaff71b929860bc
SVO <svo@qual.is>
2025-03-14 21:13:04 +1100

    Center and drop font size for More Musings on main page

--- src/app/_components/post-author.tsx
+++ src/app/_components/post-author.tsx
@@ -10,13 +10,8 @@ export function PostAuthor({ author }: Props) {
   return (
     <div className="border-t border-gray-200 dark:border-gray-700 mt-12 pt-12 mb-12">
       <div className="flex items-center justify-center flex-col mb-6">
-        <div className="mb-4">
+        <div>
           <Avatar name={author.name} picture={author.picture} />
-        </div>
-        <div className="text-lg text-center">
-          <p className="text-gray-500 dark:text-gray-400">
-            {author.name}
-          </p>
         </div>
       </div>
     </div>
