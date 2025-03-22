9f1304cd47823d3767ee98d7a340a731aac6f42d
SVO <svo@qual.is>
2025-03-22 11:30:10 +1100

    Moving published date to above title

--- src/app/_components/post-header.tsx
+++ src/app/_components/post-header.tsx
@@ -12,17 +12,17 @@ type Props = {
 export function PostHeader({ title, coverImage, date, topic }: Props) {
   return (
     <div className="w-full">
+      {date && (
+        <div className="mb-4 md:mb-8 text-lg text-primary dark:text-primary text-center">
+          <p>
+            Published on <DateFormatter dateString={date} />
+          </p>
+        </div>
+      )}
       <PostTitle>{title}</PostTitle>
       {topic && (
         <div className="mb-2 md:mb-4 text-center">
           <span className="inline-block capitalize px-3 py-1 text-primary dark:text-primary font-medium border border-primary rounded-md">{topic}</span>
-        </div>
-      )}
-      {date && (
-        <div className="mb-8 md:mb-16 text-lg text-primary dark:text-primary text-center">
-          <p>
-            Published on <DateFormatter dateString={date} />
-          </p>
         </div>
       )}
     </div>
