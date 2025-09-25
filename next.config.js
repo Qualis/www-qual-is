b9297475917963382f328effb8f143dccaa4408c
SVO <svo@qual.is>
2025-09-25 18:13:56 +1000

    Attempting to minimise JS

--- next.config.js
+++ next.config.js
@@ -5,6 +5,10 @@ const nextConfig = {
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
   },
   reactStrictMode: true,
+  swcMinify: true,
+  compiler: {
+    removeConsole: process.env.NODE_ENV === 'production',
+  },
   async headers() {
     return [
       {
@@ -63,6 +67,15 @@ const nextConfig = {
       },
       {
         source: '/assets/:path*.ico',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=31536000, immutable',
+          },
+        ],
+      },
+      {
+        source: '/_next/static/:path*',
         headers: [
           {
             key: 'Cache-Control',
