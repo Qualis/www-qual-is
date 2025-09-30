536ca638bd94b55d67ecd5f0913bc0cfbab8d22a
SVO <svo@qual.is>
2025-09-30 18:33:27 +1000

    Removing contact button from footer for the moment

--- src/app/_components/footer.tsx
+++ src/app/_components/footer.tsx
@@ -52,26 +52,6 @@ export default function Footer() {
 
             <div className="flex items-center space-x-4">
               <SocialLinks />
-              <button
-                onClick={openContactModal}
-                className="bg-primary hover:bg-accent-1 hover:text-primary border border-primary text-white font-bold py-2 px-4 duration-200 transition-colors"
-                aria-label="Contact"
-              >
-                <svg
-                  className="h-5 w-5"
-                  fill="none"
-                  stroke="currentColor"
-                  viewBox="0 0 24 24"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
-                  />
-                </svg>
-              </button>
             </div>
           </div>
         </div>
